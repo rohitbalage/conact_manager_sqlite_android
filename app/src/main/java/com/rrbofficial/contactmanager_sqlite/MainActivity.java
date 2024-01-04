f4dd9bab17dae434fec7b66e393682347d62fa68
Rohit Balage <thatcomputerboy26@gmail.com>
2024-01-04 11:33:21 -0500

    Initial commit for SQLite

---
+++ app/src/main/java/com/rrbofficial/contactmanager_sqlite/MainActivity.java
@@ -0,0 +1,16 @@
+package com.rrbofficial.contactmanager_sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/rrbofficial/contactmanager_sqlite/contact.java
@@ -0,0 +1,8 @@
+package com.rrbofficial.contactmanager_sqlite;
+
+public class contact {
+    private int id;
+    private String name;
+    private  String phoneNumber;
+
+}
